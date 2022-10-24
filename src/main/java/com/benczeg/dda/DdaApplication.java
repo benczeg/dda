a4fc2aa261ab6320cbc924c668b06c30dd3e5548
benczeg <benczegabor24@gmail.com>
2022-10-24 14:45:27 +0200

    spring boot init

---
+++ src/main/java/com/benczeg/dda/DdaApplication.java
@@ -0,0 +1,13 @@
+package com.benczeg.dda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benczeg/dda/DdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benczeg.dda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
